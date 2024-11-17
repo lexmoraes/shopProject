c135c3f8eb722d36ab24c7fcfe64c6bc8466051a
alef-monteiro <monteiro.alef0@gmail.com>
2024-11-17 18:26:10 -0400

    feat: correção ORD-002 -urls corrigidas, templates.

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Similar paths for `produtos` and `vendas`
 
     #Produtos (Alef)
-    path('clientes/', views.cliente_list, name='cliente_list'),
-    path('clientes/novo/', views.cliente_form, name='cliente_create'),
-    path('clientes/<int:id>/', views.cliente_form, name='cliente_update'),
+    path('produtos/', views.produto_list, name='produto_list'),
+    path('produtos/novo/', views.produto_form, name='produto_create'),
+    path('produtos/<int:id>/', views.produto_form, name='produto_update'),
 ]
